8b437576694b17a0fd33e2a4c1c4430a9dfcc838
johntheo <john.theo.souza@gmail.com>
2015-07-19 21:17:26 -0300

    GPS, icon e splash
    Monitorando posição com o GPS, alteração do icon e da splash

--- www/js/app.js
+++ www/js/app.js
@@ -5,69 +5,72 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
-    .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleLightContent();
-        }
-    });
+.run(function($ionicPlatform, $ionicPlatform, $cordovaGeolocation, geoLocation) {
+  $ionicPlatform.ready(function() {
+
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+
+  });
+
 })
 
-    .config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
 
-    // setup an abstract state for the tabs directive
-        .state('tab', {
-        url: "/tab",
-        abstract: true,
-        templateUrl: "templates/tabs.html"
-    })
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
 
-    // Each tab has its own nav history stack:
-        .state('tab.bus', {
-        url: '/bus',
-        views: {
-            'tab-bus': {
-                templateUrl: 'templates/tab-bus.html',
-                controller: 'MapController'
-            }
-        }
-    })
-    
-        .state('tab.user', {
-        url: '/user',
-        views: {
-            'tab-user': {
-                templateUrl: 'templates/tab-user.html',
-                controller: 'UserCtrl'
-            }
-        }
-    })
-    
-        .state('tab.config', {
-        url: '/config',
-        views: {
-            'tab-config': {
-                templateUrl: 'templates/tab-config.html',
-                controller: 'ConfigCtrl'
-            }
-        }
-    });
+  // Each tab has its own nav history stack:
+  .state('tab.bus', {
+    url: '/bus',
+    views: {
+      'tab-bus': {
+        templateUrl: 'templates/tab-bus.html',
+        controller: 'MapController'
+      }
+    }
+  })
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/bus');
+  .state('tab.user', {
+    url: '/user',
+    views: {
+      'tab-user': {
+        templateUrl: 'templates/tab-user.html',
+        controller: 'UserCtrl'
+      }
+    }
+  })
+
+  .state('tab.config', {
+    url: '/config',
+    views: {
+      'tab-config': {
+        templateUrl: 'templates/tab-config.html',
+        controller: 'ConfigCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/bus');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,111 +1,138 @@
-var map;
-
 angular.module('starter.controllers', [])
 
-    .controller('ConfigCtrl', function($scope) {
-    $scope.settings = {
-        enableGPS: true
-    };
+.controller('ConfigCtrl', function($scope) {
+  $scope.settings = {
+    enableGPS: true
+  };
 })
-    
-    .controller('UserCtrl', function($scope,$http,$ionicLoading) {
-    $scope.show = false;
-    
-    $http.get('http://servidor-musikais.rhcloud.com/util/list/onibus').
+
+.controller('UserCtrl', function($scope, $http, $ionicLoading) {
+  $scope.show = false;
+
+  $http.get('http://servidor-musikais.rhcloud.com/util/list/onibus').
+  success(function(data) {
+    $scope.onibus = data;
+  });
+
+  $scope.change = function(bus) {
+    $http.get('http://servidor-musikais.rhcloud.com/busUserContext/idOnibus=' + bus.id).
     success(function(data) {
-        $scope.onibus = data;
-        $scope.bus = onibus[1];
+      $scope.show = true;
+      $scope.busContext = data;
+      $http.get('http://servidor-musikais.rhcloud.com/util/list/imagens/idRegiao=' + data.regiao.id).
+      success(function(data2) {
+        $scope.imagens = data2;
+      });
     });
-    
-    $scope.change = function(bus) {
-        $http.get('http://servidor-musikais.rhcloud.com/busUserContext/idOnibus='+bus.id).
-        success(function(data) {
-            $scope.show = true;
-            $scope.busContext = data;
-            $http.get('http://servidor-musikais.rhcloud.com/util/list/imagens/idRegiao='+data.regiao.id).
-            success(function(data2) {
-                $scope.imagens = data2;
-            }); 
-        });
-    };
+  };
 
-    $scope.vote = function(bus,idVoto){
-        $http.get('http://servidor-musikais.rhcloud.com/busUserContext/rate/idOnibus='+bus.id+'&idVoto='+idVoto).
-        success(function(data) {
-            $ionicLoading.show({ template: data.message, noBackdrop: true, duration: 2000 });
-        });
-    };
+  $scope.vote = function(bus, idVoto) {
+    $http.get('http://servidor-musikais.rhcloud.com/busUserContext/rate/idOnibus=' + bus.id + '&idVoto=' + idVoto).
+    success(function(data) {
+      $ionicLoading.show({
+        template: data.message,
+        noBackdrop: true,
+        duration: 2000
+      });
+    });
+  };
 })
 
-    .controller('MapController', function($scope, $ionicLoading, $compile, $http) {
+.controller('MapController', function($scope, $ionicLoading, $compile, $http) {
+  $scope.initialize = function() {
     $http.get('http://servidor-musikais.rhcloud.com/util/list/motorista').
     success(function(data) {
-        $scope.motoristas = data;
-        $scope.motorista = motoristas[1];
+      $scope.motoristas = data;
     });
-
     $http.get('http://servidor-musikais.rhcloud.com/util/list/onibus').
     success(function(data) {
-        $scope.onibus = data;
-        $scope.bus = onibus[1];
+      $scope.onibus = data;
     });
 
-    $scope.initialize = function() {
-        var myLatlng = new google.maps.LatLng(-25.428877, -49.271377);
+    var options = {
+      frequency: 1000,
+      timeout: 30000
+    };
+    watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+    initMap();
+  }
 
-        //Marker + infowindow + angularjs compiled ng-click
-        var contentString = "<div><a ng-click='clickTest()'>Olá!</a></div>";
-        var compiled = $compile(contentString)($scope);
+  function initMap() {
+    var myLatlng = new google.maps.LatLng(-25.428877, -49.271377);
 
-        var infowindow = new google.maps.InfoWindow({
-            content: compiled[0]
+    var mapOptions = {
+      center: myLatlng,
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    $scope.map = new google.maps.Map(document.getElementById("map"),
+      mapOptions);
+
+
+    $scope.marker = new google.maps.Marker({
+      position: myLatlng,
+      map: $scope.map,
+      icon: 'img/bus.png',
+      title: 'Musikais Curitiba'
+    });
+
+    //INIT PONTOS
+    $http.get('http://servidor-musikais.rhcloud.com/recommendation/get/regions').
+    success(function(data) {
+      $scope.pontos = data;
+      for (var ponto in data) {
+        angular.forEach($scope.pontos, function(ponto) {
+          var regiaoOptions = new google.maps.Circle({
+            strokeColor: '#33cd5f',
+            strokeOpacity: 0.1,
+            strokeWeight: 0.1,
+            fillColor: '#33cd5f',
+            fillOpacity: 0.06,
+            map: $scope.map,
+            center: new google.maps.LatLng(ponto.latitude, ponto.longitude),
+            radius: ponto.raio
+
+          });
         });
+      }
+    });
 
-        var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById("map"),
-                                  mapOptions);
+    //Marker + infowindow + angularjs compiled ng-click
+    var contentHead = "<div>";
+    var content = "Jardineira Curitiba";
+    var contentBottom = "</div>";
+    var contentString = contentHead + content + contentBottom;
+    var compiled = $compile(contentString)($scope);
 
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            title: 'Musikais Curitiba'
-        });
-
-        //INIT PONTOS
-        $http.get('http://servidor-musikais.rhcloud.com/recommendation/get/regions').
-        success(function(data) {
-            $scope.pontos = data;
-            for (var ponto in data) {
-                angular.forEach($scope.pontos, function(ponto) {
-                    var regiaoOptions = new google.maps.Circle({
-                        strokeColor: '#33cd5f',
-                        strokeOpacity: 0.1,
-                        strokeWeight: 0.1,
-                        fillColor: '#33cd5f',
-                        fillOpacity: 0.06,
-                        map: map,
-                        center: new google.maps.LatLng(ponto.latitude, ponto.longitude),
-                        radius: ponto.raio
-
-                    });
-                });
-            }
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map, marker);
-        });
+    var infowindow = new google.maps.InfoWindow({
+      content: compiled[0]
+    });
+    google.maps.event.addListener($scope.marker, 'click', function() {
+      if ($scope.marker.getAnimation() != null) {
+        $scope.marker.setAnimation(null);
+      } else {
+        $scope.marker.setAnimation(google.maps.Animation.BOUNCE);
+      }
+      infowindow.open($scope.map, $scope.marker);
+    });
 
 
-        $scope.map = map;
-        
-    }
-    //google.maps.event.addDomListener(window, 'load', initialize);
+  }
+
+  function onSuccess(position) {
+    $scope.longitude = position.coords.longitude;
+    $scope.latitude = position.coords.latitude;
+    var element = document.getElementById('info');
+    element.innerHTML = 'Latitude: ' + $scope.latitude + ' | ' + 'Longitude: ' + $scope.longitude;
+    $scope.marker.setPosition(new google.maps.LatLng($scope.latitude, $scope.longitude));
+    $scope.map.panTo(new google.maps.LatLng($scope.latitude, $scope.longitude));
+
+  }
+
+  function onError(error) {
+    alert('code: ' + error.code + '\n' +
+      'message: ' + error.message + '\n');
+  }
 
 
 });
